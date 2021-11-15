0efe694f096b3245cfa08f66ccbd1ce553d3993e
Chaoching Vang <chaoching.vang@gmail.com>
2021-11-15 11:57:02 -0600

    if user already logged in, push to /journal

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -88,7 +88,7 @@ function App() {
             {user.id ?
               // If the user is already logged in, 
               // redirect to the /user page
-              <Redirect to="/user" />
+              <Redirect to="/journal" />
               :
               // Otherwise, show the login page
               <LoginPage />
