1e951401d2214cf01b13295df2009d5a6835478f
Chaoching Vang <chaoching.vang@gmail.com>
2021-11-15 15:11:48 -0600

    rendered details onto dom and added back btn

--- src/components/Journal/JournalDetails/JournalDetails.jsx
+++ src/components/Journal/JournalDetails/JournalDetails.jsx
@@ -1,9 +1,11 @@
 import { useSelector } from "react-redux";
 import { format } from 'date-fns';
+import { useHistory } from "react-router";
 
 
 function JournalDetails() {
 
+    const history = useHistory();
     const journalEntry = useSelector(store => store.journal.setSelectedEntry)
 
     console.log(journalEntry.name);
@@ -19,12 +21,15 @@ function JournalDetails() {
                 </>
             :
             <>
-                {JSON.stringify(journalEntry)}
+                <button onClick={() => history.push('/journal')}>Back to Journal</button>
 
                 <img src={journalEntry.image_url} />
                 <h2>{journalEntry.fishName}</h2>
                 <h2>Caught with: {journalEntry.lureName}</h2>
                 <h2>Caught on: {format(new Date(journalEntry.date), 'MM-dd-yyyy')}</h2>
+                <h2>Length: {journalEntry.length}</h2>
+                <h2>Weight: {journalEntry.weight} </h2>
+                <h2>Additional Comments: {journalEntry.comments} </h2>
             </>
             }
     </>)
