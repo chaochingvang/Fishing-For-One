8d97175a7c9eb203cc2ffb09b61a50ba0d58ac7f
Chaoching Vang <chaoching.vang@gmail.com>
2021-11-15 13:37:02 -0600

    created journal router w/ authentication GET request

--- server/routes/journal.router.js
+++ server/routes/journal.router.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     // GET route code here
-    console.log('in /router GET request');
+    console.log('in /journal GET router');
 
-    
+    let queryText = `SELECT * FROM "journal";`;
+
+    pool
+        .query(queryText)
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            console.error(`ERROR! /journal GET router`, err);
+            res.sendStatus(500);
+        });
 });
 
 /**
