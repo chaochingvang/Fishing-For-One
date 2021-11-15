3f23e92ed9e040b7b839d55617577e5be95292c9
Chaoching Vang <chaoching.vang@gmail.com>
2021-11-15 13:59:16 -0600

    grabbed journal list reducer onto journalList and mapped into journalItem

--- src/components/Journal/JournalList/JournalList.jsx
+++ src/components/Journal/JournalList/JournalList.jsx
@@ -1,10 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+
+import JournalItem from '../JournalItem/JournalItem';
+
 function JournalList() {
+    const dispatch = useDispatch();
 
-    
+    useEffect(() => {
+        dispatch({ type: `FETCH_JOURNAL` });
+    }, [])
 
+    const journalList = useSelector(store => store.journal.setJournal);
+
+    console.log(`this is journal`, journalList);
 
     return (<>
         <h1>Journal List!</h1>
+
+        {JSON.stringify(journalList)}
+
+        {journalList?.map(journal => (
+            <JournalItem
+                key={journal.id}
+                journal={journal}
+            />
+        ))}
     </>)
 }
 
