858a04a4bcd76313d8ad07ea9b7a4f7fc8c1257a
Chaoching Vang <chaoching.vang@gmail.com>
2021-11-27 13:00:21 -0600

    styled journal details page

--- src/components/Journal/JournalDetails/JournalDetails.jsx
+++ src/components/Journal/JournalDetails/JournalDetails.jsx
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux";
 import { format } from 'date-fns';
 import { useHistory } from "react-router";
+import { Redirect } from "react-router-dom";
 
-import { Button, Box, Card } from '@mui/material';
+import { Button, Box, Card, Typography } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-import './JournalDetails.css';
 
 function JournalDetails() {
 
@@ -13,42 +14,48 @@ function JournalDetails() {
 
 
     return (<>
-        <h1>JOURNAL DETAILS</h1>
-
         {(journalEntry.name === 'empty')
             ?
             <>
-                <p>NO SELECTED ENTRY</p>
+                <Redirect to="/404" />
             </>
             :
             <>
-                <Button variant="contained" onClick={() => history.push('/journal')}>Back to Journal</Button>
-
+                <Box sx={{padding: "1em"}}>
+                    <Button
+                        startIcon={<ArrowBackIcon />}
+                        variant="contained"
+                        onClick={() => history.push('/journal')}>
+                        Back to Journal
+                    </Button>
+                </Box>
+                    
                 <Box sx={{
-                    width: '90%',
+                    width: "90%",
                     // height: 300,
-                    margin: 'auto',
-                    padding: '1em',
-                    border: '1px solid'
+                    margin: "auto",
+                    padding: "1em"
                 }}>
+                    <Card variant="outlined" >
+                        <img src={journalEntry.image_url} />
+                    </Card>
                     <div className="details">
-                        <Card
-                            variant="outlined"
+                        <Box
                             sx={{
-                                width: '100%',
-                                height: '100%',
-                                margin: 'auto',
-                                backgroundColor: '#b2dfdb'
+                                width: "80%",
+                                margin: "auto",
+                                textAlign: "left",
+                                padding: "1em",
                             }}>
-
-                            <img src={journalEntry.image_url} />
-                            <h2>{journalEntry.fishName}</h2>
-                            <h2>Caught with: {journalEntry.lureName}</h2>
-                            <h2>Caught on: {format(new Date(journalEntry.date), 'MM-dd-yyyy')}</h2>
-                            <h2>Length: {journalEntry.length}</h2>
-                            <h2>Weight: {journalEntry.weight} </h2>
-                            <p>Additional Comments: {journalEntry.comments} </p>
-                        </Card>
+                            <Typography variant="body1">
+                                Fish Type: {journalEntry.fishName} <br />
+                                Lure Used: {journalEntry.lureName} <br />
+                                Date Caught: {format(new Date(journalEntry.date), 'MM-dd-yyyy')} <br />
+                                Length: {journalEntry.length ? `${journalEntry.length} in` : `Not measured`}<br />
+                                Weight: {journalEntry.weight ? `${journalEntry.weight} lbs` : `Not measured`}<br />
+                                {journalEntry.comments ? `Additional Comments: ${journalEntry.comments}` : ``}
+                            </Typography>
+                        </Box>
                     </div>
 
                 </Box>
